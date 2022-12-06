5f2a3931c23bf6cce2dd16be6e36dba3ff9a7dbc
Colt Street <colt.street@dataventures.com>
2022-12-06 09:56:01 -0500

    update to google analytics 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { OwlOptions } from 'ngx-owl-carousel-o';
 import { trigger, transition, animate, style } from '@angular/animations'
 
 declare var jQuery: any;
+declare const gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -21,6 +23,15 @@ declare var jQuery: any;
   ]
 })
 export class AppComponent implements OnInit {
+
+  constructor(public router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'G-8SY773SK8B', { 'page_path': event.urlAfterRedirects });
+      }      
+    })
+  }
+
   title = 'coltstreet.com';
 
   showAboutMe = false;
