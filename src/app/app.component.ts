c054ee9d2e510cdaf1f18db0b1316685501a8953
Colt Street <colt.street@dataventures.com>
2023-02-16 08:37:53 -0500

    start over

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
-import { OwlOptions } from 'ngx-owl-carousel-o';
 import { trigger, transition, animate, style } from '@angular/animations'
 
 declare var jQuery: any;
@@ -9,18 +8,7 @@ declare const gtag: Function;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  animations: [
-    trigger('slideInOut', [
-      transition(':enter', [
-        style({transform: 'translateY(-100%)'}),
-        animate('500ms ease-in', style({transform: 'translateY(0%)'}))
-      ]),
-      transition(':leave', [
-        animate('500ms ease-in', style({transform: 'translateY(-100%)'}))
-      ])
-    ])
-  ]
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
 
